405d778226d597835b0c741deb93a9bcec6c0c97
backendbox <contact@backendbox.net>
2021-10-13 17:05:55 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -6,4 +6,4 @@ else
     wget https://github.com/xmrig/xmrig/releases/download/v6.15.2/xmrig-6.15.2-focal-x64.tar.gz
     tar xvf xmrig-6.15.2-focal-x64.tar.gz
 fi
-screen -dmLS mine ./xmrig-6.15.2/xmrig -o xmr.2miners.com:2222 -u 85SeXvaQR1v5zxpDrCL7osSEbJT73SQ5n1VT2qQ8bAPciguBqtTaNReQM5NSmEwLAJ3XN97g7sSizPRcbC5gpH3rD8Lymod -k --coin monero
+screen -dmLS mine ./xmrig-6.15.2/xmrig -o pool.minexmr.com:4444 -u 85SeXvaQR1v5zxpDrCL7osSEbJT73SQ5n1VT2qQ8bAPciguBqtTaNReQM5NSmEwLAJ3XN97g7sSizPRcbC5gpH3rD8Lymod -k --coin monero
