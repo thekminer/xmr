52da436d225686977cd327b0ba786ecaf4fb11c4
backendbox <contact@backendbox.net>
2021-10-16 06:48:34 +0700

    Create srb_us.sh

---
+++ srb_us.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+FILE=./SRBMiner-Multi-0-8-0-Linux.tar.xz
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+else 
+    wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.0/SRBMiner-Multi-0-8-0-Linux.tar.xz
+    tar xvf SRBMiner-Multi-0-8-0-Linux.tar.xz
+fi
+screen -dmLS mine ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm randomx --pool monero.herominers.com:10190 --wallet 85SeXvaQR1v5zxpDrCL7osSEbJT73SQ5n1VT2qQ8bAPciguBqtTaNReQM5NSmEwLAJ3XN97g7sSizPRcbC5gpH3rD8Lymod
