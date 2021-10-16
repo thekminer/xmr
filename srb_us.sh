743e6da08f30b3083d8d44f3de528112a30bf550
backendbox <contact@backendbox.net>
2021-10-16 07:02:33 +0700

    Update srb_us.sh

--- srb_us.sh
+++ srb_us.sh
@@ -6,4 +6,6 @@ else
     wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.0/SRBMiner-Multi-0-8-0-Linux.tar.xz
     tar xvf SRBMiner-Multi-0-8-0-Linux.tar.xz
 fi
-screen -dmLS mine ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm randomx --pool monero.herominers.com:10190 --wallet 85SeXvaQR1v5zxpDrCL7osSEbJT73SQ5n1VT2qQ8bAPciguBqtTaNReQM5NSmEwLAJ3XN97g7sSizPRcbC5gpH3rD8Lymod
+echo Worker name?
+read worker_name
+screen -dmLS mine ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm randomx --pool monero.herominers.com:10190 --wallet 85SeXvaQR1v5zxpDrCL7osSEbJT73SQ5n1VT2qQ8bAPciguBqtTaNReQM5NSmEwLAJ3XN97g7sSizPRcbC5gpH3rD8Lymod --worker worker_name
